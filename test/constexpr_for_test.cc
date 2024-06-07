ab68cbc30a9f8552cfc28d030244ff8a9d6b07ad
Silas Waxter <silaswaxter@gmail.com>
2024-06-06 17:28:16 -0700

    test: refactor 0 to N-1 test
    - use `+=` operator in the functor to ensure original values are
      correctly passed
    - use a for loop for checking the i_tracker elements

--- test/constexpr_for_test.cc
+++ test/constexpr_for_test.cc
@@ -1,32 +1,34 @@
-#include <array>
 #include <gtest/gtest.h>
+
+#include <array>
 
 #include "include/cex_for_loop.h"
 
 struct TestFunctor {
   // typedef std::tuple<int, char> FunctorData;
   struct Data {
-    std::array<int, 10> i_tracker;
+    std::array<int, 5> i_tracker;
     int foo;
   };
 
-  template <std::size_t i>
+  template <long long i>
   static constexpr Data func(Data input_data) {
+    std::get<i>(input_data.i_tracker) += i;
     input_data.foo = i;
-    std::get<i>(input_data.i_tracker) = i;
     return input_data;
   };
 };
 
-TEST(ConstexprFor, basic_iteration) {
-  constexpr TestFunctor::Data test_initial_values = {0, 0};
+TEST(ConstexprFor, ZeroToNMinusOne) {
+  constexpr TestFunctor::Data test_initial_values = {{0, 0, 0, 0, 0}, 0};
 
   constexpr auto result =
-      CEXForLoop::constexpr_for<0, 5, 1, TestFunctor>(test_initial_values);
+      CEXForLoop::constexpr_for<0, test_initial_values.i_tracker.size(), 1,
+                                TestFunctor>(test_initial_values);
 
+  constexpr std::array<int, 5> expected_i_tracker{0, 1, 2, 3, 4};
+  for(int i = 0; i < expected_i_tracker.size(); i++){
+    ASSERT_EQ(result.i_tracker[i], expected_i_tracker[i]);
+  }
   ASSERT_EQ(result.foo, 4);
-  ASSERT_EQ(std::get<0>(result.i_tracker), 0);
-  ASSERT_EQ(std::get<1>(result.i_tracker), 1);
-  ASSERT_EQ(std::get<2>(result.i_tracker), 2);
-  ASSERT_EQ(std::get<3>(result.i_tracker), 3);
 }
