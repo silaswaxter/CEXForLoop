943d26fc8a106ed165d12b87c696963ab570db49
Silas Waxter <silaswaxter@gmail.com>
2024-06-18 15:16:57 -0700

    feat: implement exponential template instantiation
    Exponential template instantiation is a method for subverting compiler
    template instantiation depth limits. When a for loop requiring more than
    `CEX_FOR_LOOP_MAX_TEMPLATE_DEPTH` (an override-able `#define`) is
    detected, exponential instantiation is invoked. An N-tree (N=8) is
    created; each node iterates (via linear template instantiation) as much
    as possible without violating `CEX_FOR_LOOP_MAX_TEMPLATE_DEPTH`.
    
    - refactor the public interface to point to this new implementation.
    - add tests for template instantiation

--- include/cex_for_loop.h
+++ include/cex_for_loop.h
@@ -9,8 +9,8 @@ template <long long start, long long end, long long inc,
           typename BoolExpressionFunctor, typename BodyFunctor>
 constexpr typename BodyFunctor::Data constexpr_for(
     typename BodyFunctor::Data initial_values) {
-  return impl::ConstexprFor<BoolExpressionFunctor, BodyFunctor>::template func<
-      start, end, inc>(initial_values);
+  return impl::ExponentialCEXForFunctor<BoolExpressionFunctor, BodyFunctor, 1>::
+      template func<start, end, inc>(initial_values);
 };
 
 }  // namespace CEXForLoop
